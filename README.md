87d27f921bb06de588f016a33905f6cc5f37c93a
g_k <gkimth08@naver.com>
2026-06-01 00:06:47 +0900

    add .nojekyll for gh-pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# animation-next-app
--- global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css';
-declare module '*.module.css';
-declare module '*.scss';
-declare module '*.module.scss';
-declare module '*.svg';
-declare module '*.png';
-declare module '*.jpg';
